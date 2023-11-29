c3cf0924d2bf9861f6e1461cafdff54067d2fc67
ANTONIO VINÍCIUS <146228058+ViniciusOliver13@users.noreply.github.com>
2023-11-28 21:48:38 -0300

    Add files via upload

---
+++ Teste-Questao-1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+void inverteVetor(int *v,int tam){
+    int i, c, num[tam];
+    c = 0;
+    for (i = 0; i < tam; i++)
+    {
+        num[i] = *(v + i);
+    }
+    for ( i = tam - 1; i >= 0; i--)
+    {
+        *(v + c) = num[i];
+        printf("%d >", *(v + c));
+        c++;
+    }
+    
+}
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+    int tam, i;
+
+    printf("Qual é o tamanho do Vetor de números inteiros? ");
+    scanf("%d", &tam);
+
+    int vetor[tam];
+
+    for (i = 0; i < tam; i++){
+        printf("Digite o número da posição %dº: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+    
+    inverteVetor(vetor,tam);
+
+    return 0;
+}
